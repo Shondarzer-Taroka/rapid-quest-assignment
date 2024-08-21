c82a2ab10d4746276f8198eb51315ee9f3d947c8
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-08-21 22:22:53 +0600

    city added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,23 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
-import useCommon from './hooks/useCommon'
 import SalesChart from './pages/SalesChart/SalesChart'
 import SalesGrowthChart from './pages/SalesGrowthChart/SalesGrowthChart'
 import NewCustomersChart from './pages/NewCustomersChart/NewCustomersChart'
 import RepeatCustomersChart from './pages/RepeatCustomersChart/RepeatCustomersChart'
 import MapView from './pages/MapView/MapView'
-import axios from 'axios'
 import CustomerLifetimeValueChart from './pages/CustomerLifetimeValueChart'
 
 function App() {
-  const [count, setCount] = useState([])
-  let axiosCommon = useCommon()
-  useEffect(() => {
-    axios.get('http://localhost:7700/api/sales-growth')
-      .then(res => {
-        setCount(res.data)
-      })
-      .catch(err => {
-        console.log(err);
-
-      })
-  }, [axiosCommon])
-
-  console.log(count);
+  
 
   return (
 
     <>
 
-      HI , This is App page
+        <h1>Rapid Quest</h1>
 
       <SalesChart />
-      <h1>Sales Growth Over Time</h1>
+      <h1>Sales Growth Over Time Chart</h1>
       <SalesGrowthChart></SalesGrowthChart>
       <h1>New Customers Chart</h1>
       <NewCustomersChart />
@@ -40,7 +25,7 @@ function App() {
       <h1>Repeat Customers Chart </h1>
       <RepeatCustomersChart></RepeatCustomersChart>
         
-        {/* <h1>customer</h1> */}
+        <h1>Customer Lifetime Value by Cohorts Chart</h1>
       <CustomerLifetimeValueChart></CustomerLifetimeValueChart>
       <h1>Ditribution</h1>
       <MapView></MapView>
