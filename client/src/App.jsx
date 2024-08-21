1e104789a501676fedb6caeb43a990b8dbd5a593
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-08-21 13:31:35 +0600

    quarterly data aggregation completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import useCommon from './hooks/useCommon'
-import SalesChart from './pages/SalesChart/SalesChart'
-import SalesGrowthChart from './pages/SalesGrowthChart/SalesGrowthChart'
-import NewCustomersChart from './pages/NewCustomersChart/NewCustomersChart'
-import RepeatCustomersChart from './pages/RepeatCustomersChart/RepeatCustomersChart'
-import MapView from './pages/MapView/MapView'
+// import SalesChart from './pages/SalesChart/SalesChart'
+// import SalesGrowthChart from './pages/SalesGrowthChart/SalesGrowthChart'
+// import NewCustomersChart from './pages/NewCustomersChart/NewCustomersChart'
+// import RepeatCustomersChart from './pages/RepeatCustomersChart/RepeatCustomersChart'
+// import MapView from './pages/MapView/MapView'
 import axios from 'axios'
-import CustomerLifetimeValueChart from './pages/CustomerLifetimeValueChart'
+// import CustomerLifetimeValueChart from './pages/CustomerLifetimeValueChart'
 
 function App() {
   const [count, setCount] = useState([])
   let axiosCommon = useCommon()
   useEffect(() => {
-    axios.get('http://localhost:7700/orders')
+    axios.get('http://localhost:7700/api/sales-growth')
       .then(res => {
         setCount(res.data)
       })
@@ -23,7 +23,7 @@ function App() {
       })
   }, [axiosCommon])
 
-  // console.log(count);
+  console.log(count);
 
   return (
 
@@ -31,7 +31,7 @@ function App() {
 
       HI , This is App page
 
-      <SalesChart />
+      {/* <SalesChart />
       <h1>Sales Growth Over Time</h1>
       <SalesGrowthChart></SalesGrowthChart>
       <h1>New Customers Chart</h1>
@@ -39,10 +39,11 @@ function App() {
 
       <h1>Repeat Customers Chart </h1>
       <RepeatCustomersChart></RepeatCustomersChart>
-      {/* <h1> CustomerLifetimeValueChart</h1> */}
+        
+        <h1>customer</h1>
       <CustomerLifetimeValueChart></CustomerLifetimeValueChart>
       <h1>Ditribution</h1>
-      <MapView></MapView>
+      <MapView></MapView> */}
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -116,10 +116,154 @@ async function run() {
 
    // // growth 
 
-    app.get('/api/sales-growth', async (req, res) => {
+    // app.get('/api/sales-growth', async (req, res) => {
     //   try {
-    //     // const ordersCollection = db.collection('shopifyOrders');
+    //     const ordersCollection = db.collection('shopifyOrders');
 
+    //     const salesGrowthData = await ordersCollection.aggregate([
+    //       {
+    //         $addFields: {
+    //           created_at_date: { $dateFromString: { dateString: "$created_at" } }
+    //         }
+    //       },
+    //       {
+    //         $facet: {
+    //           daily: [
+    //             {
+    //               $group: {
+    //                 _id: { $dateToString: { format: "%Y-%m-%d", date: "$created_at_date" } },
+    //                 totalSales: { $sum: { $toDouble: "$total_price_set.shop_money.amount" } }
+    //               }
+    //             },
+    //             { $sort: { _id: 1 } },
+    //             {
+    //               $setWindowFields: {
+    //                 sortBy: { _id: 1 },
+    //                 output: {
+    //                   previousSales: {
+    //                     $shift: {
+    //                       output: "$totalSales",
+    //                       by: -1
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             },
+    //             {
+    //               $project: {
+    //                 _id: 1,
+    //                 totalSales: 1,
+    //                 growthRate: {
+    //                   $cond: {
+    //                     if: { $eq: ["$previousSales", 0] },
+    //                     then: null,
+    //                     else: {
+    //                       $multiply: [
+    //                         { $divide: [{ $subtract: ["$totalSales", "$previousSales"] }, "$previousSales"] },
+    //                         100
+    //                       ]
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           ],
+    //           monthly: [
+    //             {
+    //               $group: {
+    //                 _id: { $dateToString: { format: "%Y-%m", date: "$created_at_date" } },
+    //                 totalSales: { $sum: { $toDouble: "$total_price_set.shop_money.amount" } }
+    //               }
+    //             },
+    //             { $sort: { _id: 1 } },
+    //             {
+    //               $setWindowFields: {
+    //                 sortBy: { _id: 1 },
+    //                 output: {
+    //                   previousSales: {
+    //                     $shift: {
+    //                       output: "$totalSales",
+    //                       by: -1
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             },
+    //             {
+    //               $project: {
+    //                 _id: 1,
+    //                 totalSales: 1,
+    //                 growthRate: {
+    //                   $cond: {
+    //                     if: { $eq: ["$previousSales", 0] },
+    //                     then: null,
+    //                     else: {
+    //                       $multiply: [
+    //                         { $divide: [{ $subtract: ["$totalSales", "$previousSales"] }, "$previousSales"] },
+    //                         100
+    //                       ]
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           ],
+    //           yearly: [
+    //             {
+    //               $group: {
+    //                 _id: { $dateToString: { format: "%Y", date: "$created_at_date" } },
+    //                 totalSales: { $sum: { $toDouble: "$total_price_set.shop_money.amount" } }
+    //               }
+    //             },
+    //             { $sort: { _id: 1 } },
+    //             {
+    //               $setWindowFields: {
+    //                 sortBy: { _id: 1 },
+    //                 output: {
+    //                   previousSales: {
+    //                     $shift: {
+    //                       output: "$totalSales",
+    //                       by: -1
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             },
+    //             {
+    //               $project: {
+    //                 _id: 1,
+    //                 totalSales: 1,
+    //                 growthRate: {
+    //                   $cond: {
+    //                     if: { $eq: ["$previousSales", 0] },
+    //                     then: null,
+    //                     else: {
+    //                       $multiply: [
+    //                         { $divide: [{ $subtract: ["$totalSales", "$previousSales"] }, "$previousSales"] },
+    //                         100
+    //                       ]
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           ]
+    //         }
+    //       }
+    //     ]).toArray();
+
+    //     res.json(salesGrowthData[0]);
+    //   } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ error: 'Failed to fetch sales growth data' });
+    //   }
+    // });
+
+
+    app.get('/api/sales-growth', async (req, res) => {
+      try {
+       
+    
         const salesGrowthData = await ordersCollection.aggregate([
           {
             $addFields: {
@@ -208,6 +352,54 @@ async function run() {
                   }
                 }
               ],
+              quarterly: [
+                {
+                  $group: {
+                    _id: {
+                      $concat: [
+                        { $toString: { $year: "$created_at_date" } },
+                        "-Q",
+                        {
+                          $toString: { $ceil: { $divide: [{ $month: "$created_at_date" }, 3] } }
+                        }
+                      ]
+                    },
+                    totalSales: { $sum: { $toDouble: "$total_price_set.shop_money.amount" } }
+                  }
+                },
+                { $sort: { _id: 1 } },
+                {
+                  $setWindowFields: {
+                    sortBy: { _id: 1 },
+                    output: {
+                      previousSales: {
+                        $shift: {
+                          output: "$totalSales",
+                          by: -1
+                        }
+                      }
+                    }
+                  }
+                },
+                {
+                  $project: {
+                    _id: 1,
+                    totalSales: 1,
+                    growthRate: {
+                      $cond: {
+                        if: { $eq: ["$previousSales", 0] },
+                        then: null,
+                        else: {
+                          $multiply: [
+                            { $divide: [{ $subtract: ["$totalSales", "$previousSales"] }, "$previousSales"] },
+                            100
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              ],
               yearly: [
                 {
                   $group: {
@@ -251,13 +443,14 @@ async function run() {
             }
           }
         ]).toArray();
-
-    //     res.json(salesGrowthData[0]);
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).json({ error: 'Failed to fetch sales growth data' });
-    //   }
+    
+        res.json(salesGrowthData[0]);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch sales growth data' });
+      }
     });
+    
 
 
 
